098ac807517ea9dde3cf08b80bcdb058b49cbc61
Bradly Alicea <freejumper@yahoo.com>
2014-05-15 11:26:50 -0500

    Delete parser.py

--- parser.py
+++
@@ -1,6 +0,0 @@
-from xml.dom import minidom
-xmldoc = minidom.parse('C:\items.xml')			//xml file with data to parse. 
-itemlist = xmldoc.getElementsByTagName('daughter of')	// 'item' is the semantic ID. Change w.r.t. desired output.
-s = len(itemlist)
-for s in itemlist:
-    print s.attributes['name'].value			//all attributes tagged with item ID.
